1edbce80440c591ad505323f35f300b957d3e583
sarahhcarl <sarahhcarl@gmail.com>
2013-11-27 16:52:12 +0000

    Affinity model is working! Need to implement z-test

--- analysis/Code/R_scripts/affinity_model.R
+++ analysis/Code/R_scripts/affinity_model.R
@@ -1,5 +1,8 @@
 ##Implementation of Alan Moses' affinity-based model of selection on TF binding sites
 
+#Currently takes alignments for every site which is present in the common ancestory (node 3), regardless of whether it is present in all descendants.
+#Need to filter for gaps in alignments.
+
 library(Biostrings)
 
 # Define background frequences of A, C, T and G (in this case, background Drosophila noncoding frequences from RSAT)
@@ -12,33 +15,7 @@ SoxN_matrix <- matrix(c(0.355,0.217,0.274,0.155, 0.208,0.213,0.521,0.058, 0.253,
 # Define which PWM model to use
 f <- D_matrix
 
-# Read in alignments, store as biostrings
-
-con <- file("/home/sarah/utilities/play-1.2.7/sox_sites/analysis/Sequences/site_alignments/Dichaete/5-HT1A.53C10.434.fasta")
-alignment <- readLines(con)
-
-node3 <- DNAString(alignment[3])
-node2 <- DNAString(alignment[5])
-node1 <- DNAString(alignment[7])
-mel <- DNAString(alignment[9])
-sim <- DNAString(alignment[11])
-yak <- DNAString(alignment[13])
-pse <- DNAString(alignment[15])
-
-# If strand = R, take reverse complement
-if (alignment[1] == "R") {
-  node3 <- reverseComplement(node3)
-  node2 <- reverseComplement(node2)
-  node1 <- reverseComplement(node1)
-  mel <- reverseComplement(mel)
-  sim <- reverseComplement(sim)
-  yak <- reverseComplement(yak)
-  pse <- reverseComplement(pse)
-}
-
-
-# Identify substitutions (what to do for gaps?)
-
+#Definition of find_subst function
 find_subst <- function(col, dS_total, dS_counter) {
   node3_nt <- as.character(node3[col])
   node2_nt <- as.character(node2[col])
@@ -82,8 +59,9 @@ find_subst <- function(col, dS_total, dS_counter) {
   }
   dS_list <- c(dS_total, dS_counter)
   return(dS_list)
-}
+}  
 
+#Definition of calc_dS function
 calc_dS <- function(nt1, nt2, pos, bg_freqs, D_matrix) {
   g_nt1 <- bg_freqs[, nt1]
   g_nt2 <- bg_freqs[, nt2]
@@ -93,20 +71,79 @@ calc_dS <- function(nt1, nt2, pos, bg_freqs, D_matrix) {
   return(dS)
 }
 
-#nts <- c(1:length(mel))
-##dS_list <- sapply(nts, find_subst, dS_list)
+# Read in alignments, store as biostrings
 
-
-##Trying to do: for each position, get all the values of dS for any substitution at that position, add them to a list, then repeat, always appending values to the same list.
-dS_total <- 0
-dS_counter <- 0
-for (col in 1:length(mel)) {
-  dS_list <- find_subst(col, dS_total, dS_counter)
-  dS_total <- dS_list[1] + dS_total
-  dS_counter <- dS_list[2] + dS_counter
-}
-
-dS_avg = dS_total / dS_counter
+files <- list.files(path="/home/sarah/utilities/play-1.2.7/sox_sites/analysis/Sequences/site_alignments/Dichaete", pattern=".fasta", all.files=T, full.names=T)
+for (myfile in files) {
+  #print(myfile)
+  con <- file(myfile)
+  alignment <- readLines(con)
+  
+  gap3 <- grepl("-", alignment[3])
+  gap2 <- grepl("-", alignment[5])
+  gap1 <- grepl("-", alignment[7])
+  gap_mel <- grepl("-", alignment[9])
+  gap_sim <- grepl("-", alignment[11])
+  gap_yak <- grepl("-", alignment[13])
+  gap_pse <- grepl("-", alignment[15])
+  
+  n3 <- grepl("N", alignment[3])
+  n2 <- grepl("N", alignment[5])
+  n1 <- grepl("N", alignment[7])
+  n_mel <- grepl("N", alignment[9])
+  n_sim <- grepl("N", alignment[11])
+  n_yak <- grepl("N", alignment[13])
+  n_pse <- grepl("N", alignment[15])
+  
+  if ((gap3 == TRUE) | (gap2 == TRUE) | (gap1 == TRUE) | (gap_mel == TRUE) | (gap_sim == TRUE) | (gap_yak == TRUE) | (gap_pse == TRUE)) {
+    cat(myfile, file="all_Dichaete_gaps.txt", sep=" ", fill=TRUE, append=TRUE)
+    cat("Gaps detected", file="all_Dichaete_gaps.txt", sep=" ", fill=TRUE, append=TRUE)
+    close(con)
+  } 
+  else if ((n3 == TRUE) | (n2 == TRUE) | (n1 == TRUE) | (n_mel == TRUE) | (n_sim == TRUE) | (n_yak == TRUE) | (n_pse == TRUE)) {
+    cat(myfile, file="all_Dichaete_n.txt", sep=" ", fill=TRUE, append=TRUE)
+    cat("N detected", file="all_Dichaete_n.txt", sep=" ", fill=TRUE, append=TRUE)
+    close(con)
+  }
+  else if ((gap3 == FALSE) & (gap2 == FALSE) & (gap1 == FALSE) & (gap_mel == FALSE) & (gap_sim == FALSE) & (gap_yak == FALSE) & (gap_pse == FALSE) & (n3 == FALSE) & (n2 == FALSE) & (n1 == FALSE) & (n_mel == FALSE) & (n_sim == FALSE) & (n_yak == FALSE) & (n_pse == FALSE)) {
+    
+    node3 <- DNAString(alignment[3])
+    node2 <- DNAString(alignment[5])
+    node1 <- DNAString(alignment[7])
+    mel <- DNAString(alignment[9])
+    sim <- DNAString(alignment[11])
+    yak <- DNAString(alignment[13])
+    pse <- DNAString(alignment[15])
+    
+    # If strand = R, take reverse complement
+    if (alignment[1] == "R") {
+      node3 <- reverseComplement(node3)
+      node2 <- reverseComplement(node2)
+      node1 <- reverseComplement(node1)
+      mel <- reverseComplement(mel)
+      sim <- reverseComplement(sim)
+      yak <- reverseComplement(yak)
+      pse <- reverseComplement(pse)
+    }
+    
+    
+    # Identify substitutions
+  
+    #For each position, get all the values of dS for any substitution at that position, add them to a list, then repeat, always appending values to the same list.
+    dS_total <- 0
+    dS_counter <- 0
+    for (col in 1:length(mel)) {
+      dS_list <- find_subst(col, dS_total, dS_counter)
+      dS_total <- dS_list[1] + dS_total
+      dS_counter <- dS_list[2] + dS_counter
+    }
+    
+    dS_avg = dS_total / dS_counter
+    cat(myfile, file="all_Dichaete_dS.txt", sep=" ", fill=TRUE, append=TRUE)
+    cat(dS_avg, file="all_Dichaete_dS.txt", sep=" ", fill=TRUE, append=TRUE)
+    close(con)
+  }  
+}  
 
 # Calculate dS for each substitution where a directional change can be identified
 # Take average dS for alignment(s)
